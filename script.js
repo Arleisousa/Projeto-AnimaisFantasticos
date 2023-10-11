a578fd91cb6aa455f5cd6236222061279eb6ab58
arlei sousa <arleidesoussa@gmail.com>
2023-10-11 02:45:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-//Constantemente vamos selecionar uma lista de itens do dom. A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
-
-/*const imgs = document.querySelectorAll('img');
-
-imgs.forEach(function(item, index, array){
-  console.log(item); // o item atual no loop
-  console.log(index); // o número do index
-  console.log(array); // a Array completa
-});
-*/
-
-// Mostre no console cada parágrado do site
-
 const p = document.querySelectorAll('p')
 
-p.forEach((item) => {
-    console.log(item);
-});
+p.forEach( (item) =>  {
+  console.log(item)
+})
 
-// Mostre o texto dos parágrafos no console
-
-p.forEach((item) => {
-    console.log(item.innerText);
-});
-
-// Como corrigir os erros abaixo:
-
+p.forEach( (item) =>  {
+  console.log(item.innerText)
+})
 
 const imgs = document.querySelectorAll('img');
 
@@ -32,9 +14,8 @@ imgs.forEach((item, index) => {
   console.log(item, index);
 });
 
-
 let i = 0;
-imgs.forEach( () => {
+imgs.forEach(() => {
   console.log(i++);
 });
 
