9dcb3c441e02088d68a8b08bd7a8ebc72eed5fd9
arlei sousa <arleidesoussa@gmail.com>
2023-10-11 03:32:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,2 @@
-const p = document.querySelectorAll('p')
 
-p.forEach( (item) =>  {
-  console.log(item)
-})
-
-p.forEach( (item) =>  {
-  console.log(item.innerText)
-})
-
-const imgs = document.querySelectorAll('img');
-
-imgs.forEach((item, index) => {
-  console.log(item, index);
-});
-
-let i = 0;
-imgs.forEach(() => {
-  console.log(i++);
-});
-
-imgs.forEach(() => i++);
 
