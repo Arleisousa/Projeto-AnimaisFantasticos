ab8a066157397e47492b04c061f957a816dd01ff
arlei sousa <arleidesoussa@gmail.com>
2023-10-14 03:52:16 -0300

    add navegação

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-//const img = document.querySelector('img')
-//const imgTop = img.offsetTop
+if(tabMenu.length && tabContent.length ) { 
 
-//console.log(imgTop)
+    tabContent[0].classList.add('ativo')
 
-// Retorne a soma da largura de todas as imagens
-const fodase = document.querySelectorAll('img')
-let soma = 0
-fodase.forEach((img) => {
-    soma = soma + img.offsetWidth
-
-    console.log(soma)
-})
-
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-
-const links = document.querySelectorAll('a')
-
-links.forEach((link) => {
-    const linkWidth = link.offsetWidth
-    const linkHeight = link.offsetHeight
-    if(linkWidth >= 48  && linkHeight >= 48) {
-        console.log(link, 'possui acessibilidade')
-    } else {
-        console.log(link, 'não possui')
+    function activeTab (index) {
+        tabContent.forEach((section) => {
+          section.classList.remove('ativo')
+        })
+        tabContent[index].classList.add('ativo')
     }
-})
- 
-console.log(links)
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+          activeTab(index)
+        })
+    })
 
-const browserSmall = window.matchMedia('(max-width: 720px)').matches
-
-if (browserSmall) {
-    const menu = document.querySelector('.menu')
-    menu.classList.add('menu-mobile')
 }
