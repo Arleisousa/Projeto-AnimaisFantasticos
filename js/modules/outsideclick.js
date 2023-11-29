97a404efddb9753525e43edb709cc66b19b79e43
Arlei <arleidesoussa@gmail.com>
2023-11-29 03:01:41 -0300

    add melhorias

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,21 +1,21 @@
 export default function outsideClick(element, events, callback) {
-    const html = document.documentElement;
-    const outside = element.getAttribute('data-outside');
+  const html = document.documentElement;
+  const outside = element.getAttribute('data-outside');
 
-    if (!element.hasAttribute(outside)) {
+  if (outside !== 'true') {
       events.forEach((userEvent) => {
-        setTimeout(() => html.addEventListener(userEvent, handleOutsideClick))
+          setTimeout(() => html.addEventListener(userEvent, handleOutsideClick))
       });
       element.setAttribute('data-outside', 'true');
-    }
-
-    function handleOutsideClick(event) {
-      if (!element.contains(event.target)) {
-        events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutsideClick);
-        });
-        element.removeAttribute('data-outside');
-        callback();
-      }
-    }
   }
+
+  function handleOutsideClick(event) {
+      if (!element.contains(event.target)) {
+          events.forEach((userEvent) => {
+              html.removeEventListener(userEvent, handleOutsideClick);
+          });
+          element.removeAttribute('data-outside');
+          callback();
+      }
+  }
+}
